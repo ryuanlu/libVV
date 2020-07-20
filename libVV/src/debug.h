aa706a28b4fb46efcfbe39ec8a0b9d5595a94dc3
盧瑞元 (Ruei-Yuan Lu) <RueiYuan.Lu@gmail.com>
2020-07-21 00:21:48 +0800

    Prevent from evaluating multiple times in goto_cleanup_if macro

--- libVV/src/debug.h
+++ libVV/src/debug.h
@@ -7,7 +7,7 @@
 
 void vv_print_error(const enum vv_result code, const char* filename, const int line, const char* function);
 
-#define goto_cleanup_if_failed(__result__, __goto_label__) if((result = (__result__)) != VV_SUCCESS) { vv_print_error(__result__, __FILE__, __LINE__, __FUNCTION__); goto __goto_label__; }
+#define goto_cleanup_if_failed(__result__, __goto_label__) {result = __result__; if(result != VV_SUCCESS) { vv_print_error(result, __FILE__, __LINE__, __FUNCTION__); goto __goto_label__; }}
 #define goto_cleanup_if(__expr__, __result__, __goto_label__) goto_cleanup_if_failed((__expr__) ? __result__ : VV_SUCCESS, __goto_label__ )
 
 #endif /* __DEBUG__ */
