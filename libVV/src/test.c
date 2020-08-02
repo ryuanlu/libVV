4f0f747207c070e9ba9370d4fcb0ec9bcf222b23
盧瑞元 (Ruei-Yuan Lu) <RueiYuan.Lu@gmail.com>
2020-08-03 01:11:04 +0800

    Reduce test code

--- libVV/src/test.c
+++ libVV/src/test.c
@@ -11,61 +11,56 @@
 #define COLORMAP_PATH	"../hsv.bin"
 #define COLORMAP_WIDTH	(2048)
 
+
+vv_memory* load_texture(const vv_context* context, const char* filename, const int width, const int height, const int depth, const int bpp)
+{
+	int size = width * height * depth * bpp;
+	char* data = NULL;
+	vv_memory* texture = NULL;
+	FILE* fp = NULL;
+
+
+	fp = fopen(filename, "r");
+	data = calloc(size, 1);
+	fread(data, size, 1, fp);
+	fclose(fp);
+
+	vv_memory_create
+	(
+		&texture,
+		&(const vv_memory_desc)
+		{
+			.type = VV_MEMORY_TYPE_GLES_TEXTURE,
+			.context = context,
+			.width = width,
+			.height = height,
+			.depth = depth,
+			.bytes_per_channel = bpp,
+		},
+		data
+	);
+
+	free(data);
+
+	return texture;
+}
+
+
 int main(int argc, char** argv)
 {
-	FILE* fp = NULL;
-	unsigned char* data = NULL;
 	vv_context* context = NULL;
 	vv_memory* volume = NULL;
 	vv_memory* colormap = NULL;
 	vv_visualizer* visualizer = NULL;
 
-	fp = fopen(TEST_DATA_PATH, "r");
-	data = calloc(TEST_DATA_SIZE, 1);
-	fread(data, 1, TEST_DATA_SIZE, fp);
-	fclose(fp);
-
 	vv_context_create(&context);
 
-	vv_memory_create
-	(
-		&volume,
-		&(const vv_memory_desc)
-		{
-			.type = VV_MEMORY_TYPE_GLES_TEXTURE,
-			.context = context,
-			.width = TEST_DATA_WIDTH,
-			.height = TEST_DATA_HEIGHT,
-			.depth = TEST_DATA_DEPTH,
-			.bytes_per_channel = 2,
-		},
-		data
-	);
-
-	free(data);
-
-	fp = fopen(COLORMAP_PATH, "r");
-	data = calloc(4 * COLORMAP_WIDTH, 1);
-	fread(data, 4 * COLORMAP_WIDTH, 1, fp);
-	fclose(fp);
-
-	vv_memory_create
-	(
-		&colormap,
-		&(const vv_memory_desc)
-		{
-			.type = VV_MEMORY_TYPE_GLES_TEXTURE,
-			.context = context,
-			.width = COLORMAP_WIDTH,
-			.bytes_per_channel = 4,
-		},
-		data
-	);
-
-	free(data);
+	volume = load_texture(context, TEST_DATA_PATH, TEST_DATA_WIDTH, TEST_DATA_HEIGHT, TEST_DATA_DEPTH, 2);
+	colormap = load_texture(context, COLORMAP_PATH, COLORMAP_WIDTH, 1, 1, 4);
 
 	vv_visualizer_create(context, &visualizer, VV_VISUALIZER_TYPE_3D_TEXTURE_AXIS_ALIGNED);
 	vv_visualizer_set_viewport(visualizer, 800, 800);
+
 	vv_visualizer_set_volume(visualizer, volume);
 	vv_visualizer_set_colormap(visualizer, colormap);
 	vv_visualizer_render(visualizer);
